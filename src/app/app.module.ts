14f3e05246a86667193dd8dc9a0c5b6b445788de
dangiansu <ansud.itpathsolutions@gmail.com>
2023-08-11 18:29:52 +0530

    setup angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
